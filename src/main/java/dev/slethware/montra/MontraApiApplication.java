dd0b1c618f6046bacff05a8a8a4061e5023c5357
DannylSyph3r <dopedanny001@gmail.com>
2025-08-28 20:12:59 +0100

    First/Inital Commit

---
+++ src/main/java/dev/slethware/montra/MontraApiApplication.java
@@ -0,0 +1,13 @@
+package dev.slethware.montra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/slethware/montra/MontraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.slethware.montra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
