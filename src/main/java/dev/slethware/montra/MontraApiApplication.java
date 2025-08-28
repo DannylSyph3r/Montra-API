2b145c46830e4427a6b517497681452842f02f55
DannylSyph3r <dopedanny001@gmail.com>
2025-08-28 22:45:29 +0100

    Main Runner file setup

--- src/main/java/dev/slethware/montra/MontraApiApplication.java
+++ src/main/java/dev/slethware/montra/MontraApiApplication.java
@@ -1,13 +1,60 @@
 package dev.slethware.montra;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@OpenAPIDefinition(
+		info = @Info(
+				contact = @Contact(
+						name = "Akinola Daniel",
+						email = "slethware@gmail.com",
+						url = "https://www.slethware.dev"
+				),
+				description = "OpenApi documentation for Montra API",
+				title = "Montra API Documentation",
+				version = "1.0",
+				license = @License(
+						name = "Licence name",
+						url = "https://example.com"
+				),
+				termsOfService = "Terms of service"
+		),
+		security = {
+				@SecurityRequirement(
+						name = "bearerAuth"
+				)
+		}
+)
+@SecurityScheme(
+		name = "bearerAuth",
+		description = "JWT auth description",
+		scheme = "bearer",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		in = SecuritySchemeIn.HEADER
+)
+@EnableAsync
+@EnableScheduling
+@EnableJpaAuditing(auditorAwareRef = "auditorAware", dateTimeProviderRef = "auditingDateTimeProvider")
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication
 public class MontraApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MontraApiApplication.class, args);
 	}
-
 }
---
+++ src/main/java/dev/slethware/montra/config/AppConfig.java
@@ -0,0 +1,4 @@
+package dev.slethware.montra.config;
+
+public class AppConfig {
+}
